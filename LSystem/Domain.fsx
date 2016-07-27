5c6186c1b8c1d97fac406a5850adfb4da38222f2
pezi <pezi_pink@pinksquirrellabs.com>
2016-07-27 13:41:30 +0100

    update all the things, generalise LSystems

--- LSystem/Domain.fsx
+++ LSystem/Domain.fsx
@@ -8,62 +8,158 @@ type LineSegment = {startPoint : Point; endPoint : Point; color : Color }
 let chaos = System.Random(System.DateTime.Now.Millisecond)
 
 let red = { r = 255uy; g = 0uy; b = 0uy}
+let blue = { r = 0uy; g = 0uy; b = 255uy}
+let green = { r = 0uy; g = 255uy; b = 255uy}
+
+let randomColor() = { r = uint8(chaos.Next 256);g = uint8(chaos.Next 256);b = uint8(chaos.Next 256) }
 
 type LogoCommand =
-    | Forward of float * Color option
+    | DrawForward of float 
+    | MoveForward of float
+    | ChangeColor of Color
     | Turn of float
    
 type LTurtle = 
     { angle : float
       x : float
-      y : float } 
+      y : float 
+      c : Color} 
 
 /// interprets a logo program and produces a line segment list to render
 let processTurtle turtle program =
     let rec phono output turtle = function
         | [] -> output
-        | Forward(d,color) :: t -> 
+        | ChangeColor c :: t -> phono output {turtle with c = c} t
+        | DrawForward d :: t -> 
             let rads = turtle.angle * (System.Math.PI / 180.0)
             let x = turtle.x + d * cos rads
             let y = turtle.y + d * sin rads
             let newTurtle = {turtle with x = x; y= y }
-            match color with
-            | Some c -> 
-                let seg = 
-                    { startPoint = {x = int turtle.x; y = int turtle.y}
-                      endPoint = {x = int x; y = int y}
-                      color = c }
-                phono (seg::output) newTurtle t
-            | None -> phono output newTurtle t
+            let seg = 
+                {   startPoint = {x = int turtle.x; y = int turtle.y}
+                    endPoint = {x = int x; y = int y}
+                    color = newTurtle.c }
+            phono (seg::output) newTurtle t
+            
+        | MoveForward d :: t -> 
+            let rads = turtle.angle * (System.Math.PI / 180.0)
+            let x = turtle.x + d * cos rads
+            let y = turtle.y + d * sin rads
+            let newTurtle = {turtle with x = x; y= y }
+            phono output newTurtle t
+
         | Turn delta :: t -> 
             let d = turtle.angle + delta
             let d =
                 // warp around logic
                 if delta > 0.0 && d > 360.0 then d - 360.0
-                elif delta < 0.0 && d < 0.0 then d + 360.0
+                elif delta < 0.0 && d < 0.0 then 360.0 + d
                 else d
             phono output {turtle with angle = d} t
 
     List.rev(phono [] turtle program)
 
+    
+type LSystem = {
+    Axiom : string
+    Productions : char -> string
+    Actions : int  -> char -> (LogoCommand list) option
+}
 
-let lsystem (axiom:string) iterations =
-    let rec juan (current:string) iteration =
-        if iteration = iterations then current
+let processLsystem max lsystem =
+    // first we perform the l-system generation
+    // fast imperative generator
+    let rec gen (current:string) iteration =
+        if iteration = max then current
         else
             let sb = System.Text.StringBuilder()
             for x in current.ToCharArray() do
-                if x = 'A' then sb.Append("ABA") |> ignore
-                elif x = 'B' then sb.Append("BBB") |> ignore
-                else sb |> ignore
-            juan (sb.ToString()) (iteration+1)
-    juan axiom 0
+                sb.Append(lsystem.Productions x) |> ignore
+            gen (sb.ToString()) (iteration+1)
+              
+    let finish = gen lsystem.Axiom 0
 
-let convertToTurtle (bla:string) =
-     [for x in bla.ToCharArray() do
-         match x with
-         | 'A' -> yield Forward(20.0, Some({r=255uy;g=0uy;b=0uy}))
-         | _  -> yield Forward(20.0, None)
-         
-                                ]
-                                
+    // now convert to turtle commands
+    finish.ToCharArray() |> List.ofArray |> List.choose (lsystem.Actions max) |> List.collect id
+
+let cantor width = {
+    Axiom = "A"
+    Productions = function 'A' -> "ABA" | _ -> "BBB"
+    Actions = 
+        fun max c -> 
+            let length =   
+                let width = float width
+                if max = 0 then width 
+                else width / (System.Math.Pow(3.0, float max))
+     
+            match c with 
+            | 'A' -> Some <| [DrawForward(length)]
+            | _ -> Some <| [MoveForward(length)]
+    
+}
+
+let sierpinski width = {
+    Axiom = "A"
+   
+    Productions =
+        function
+        | 'A' -> "+B-A-B+" 
+        | 'B' -> "-A+B+A-" 
+        | c -> string c
+
+    Actions = 
+        fun max c -> 
+            let length =   
+                let width = float width
+                if max = 0 then width 
+                else width / (System.Math.Pow(2.0, float max))
+     
+            match c with
+            | 'A' -> Some <| [DrawForward(length)]
+            | 'B' -> Some <| [MoveForward(length)]
+            | '+' -> Some <| [Turn 60.0]
+            | '-' -> Some <| [Turn -60.0]
+            | _ -> None
+}
+
+
+
+let koch = {
+    Axiom = "F"
+   
+    Productions = 
+        function
+        | 'F' -> "F+F-F-F+F" 
+        | c -> string c
+
+    Actions = 
+        fun max c -> 
+            let length = 2.0
+            match c with
+            | 'F' -> Some <| [DrawForward(length)]
+            | '+' -> Some <| [Turn 90.0]
+            | '-' -> Some <| [Turn -90.0]
+            | _ -> None
+}
+
+let dragColours = [for x in 0..20 -> x,randomColor()] |> dict
+
+let dragon = {
+    Axiom = "FX"
+   
+    Productions = 
+        function
+        | 'X' -> "X+YF+" 
+        | 'Y' -> "-FX-Y"
+        | c -> string c
+
+    Actions = 
+        fun max c -> 
+            let length = 5.0
+            match c with
+            | 'F' -> Some <| [DrawForward(length)]
+            | '+' -> Some <| [Turn 90.0]
+            | '-' -> Some <| [Turn -90.0]
+            | _ -> None
+}
+
